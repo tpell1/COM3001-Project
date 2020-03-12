f1e0e14c97daa13f653487c87caa8f1b08203bbe
tpell1 <tpell1@sheffield.ac.uk>
2020-03-12 12:00:09 +0000

    initial commit

---
+++ @human/human.m
@@ -0,0 +1,48 @@
+classdef human
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Sociability;
+        age;
+        fatality;
+        reproduction;
+        migration;
+        pos;
+        speed;
+        current_village;
+        immunity;
+    end
+    
+    methods
+        % CONSTRUCTOR ====
+        % params 
+        % Sociability, age, fatality, reproduction, migration,
+        % current_village, speed, pos
+        function obj = human(varargin) 
+            switch nargin
+                case 0
+                    obj.sociability = [];
+                    obj.age = [];
+                    obj.fatality = [];
+                    obj.reproduction = [];
+                    obj.migration = [];
+                    obj.current_village = 1;
+                    obj.speed = 0;
+                    obj.pos = [];
+                    obj.immunity = [];
+                case 9
+                    obj.sociability = varargin{1};
+                    obj.age = varargin{2};
+                    obj.fatality = varargin{3};
+                    obj.reproduction = varargin{4};
+                    obj.migration = varargin{5};
+                    obj.current_village = varargin{6};
+                    obj.speed = varargin{7};
+                    obj.pos = varargin{8};
+                    obj.immunity = varargin{9};
+            end
+        end
+    end
+end
+
