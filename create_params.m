7d71d902d8015da183b957c4612b365b6ed506f6
Travis Pell <travispell@Traviss-Air.lan>
2020-04-23 09:24:34 +0100

    updated params

--- create_params.m
+++ create_params.m
@@ -8,16 +8,13 @@ function create_params
 
 global PARAM
 
-    PARAM.H_SPD=5;              %speed of movement - units per itn (human)
-    PARAM.IH_SPD=1;             %speed of movement - units per itn (infected_human)
     PARAM.D_MORTALITY=30;      %how deadly pathogen is
-    PARAM.MORTALITY=0.002;
-    PARAM.BR_AGE=30;
-    PARAM.IMMUNE_STRENGTH=5;
-    PARAM.N_OF_VILLAGES=4;       %Number of villages
+    PARAM.MORTALITY=0.002;      %how likely a healthy agent is to die on each iteration
+    PARAM.IMMUNE_STRENGTH=0;    %How likely an infected agent is to be cured (out of 100)
+    PARAM.N_OF_VILLAGES=1;       %Number of villages
     PARAM.SOCIABILITY=5;         %Sociability
-    PARAM.D_SOCIAL_F=0.33;        %how much pathogen infects sociability
-    PARAM.D_CONTAGIOUS=0.08;      %how contagious pathogen is
+    PARAM.D_SOCIAL_F=0.1;        %how much pathogen infects sociability
+    PARAM.D_CONTAGIOUS=0.05;      %how contagious pathogen is
     PARAM.H_MIGRATE_FREQ=0.1;   %how often a normal human changes village
     PARAM.I_MIGRATE_FREQ=0.05;   %how often an infected human changes village
     
